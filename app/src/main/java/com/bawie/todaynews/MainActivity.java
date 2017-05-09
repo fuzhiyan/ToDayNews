fe2ff275c1da5b98f4697c7f234cfe8dc4bd1c5e
fuzhiyan <fuzhiyan_001@163.com>
2017-05-09 20:11:42 +0800

    注释

--- app/src/main/java/com/bawie/todaynews/MainActivity.java
+++ app/src/main/java/com/bawie/todaynews/MainActivity.java
@@ -8,7 +8,7 @@ import com.bawie.todaynews.fragment.MentLeftFragment;
 import com.bawie.todaynews.fragment.MenuRightFragment;
 import com.bwei.slidingmenu.SlidingMenu;
 import com.bwei.slidingmenu.app.SlidingFragmentActivity;
-
+//这是主页面
 public class MainActivity extends SlidingFragmentActivity {
     private SlidingMenu slidingMenu;
     @Override
