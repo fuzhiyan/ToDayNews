306a260b374e298e1a4516741138096a0ed69236
fuzhiyan <fuzhiyan_001@163.com>
2017-05-10 00:32:00 +0800

    qq登录

--- app/src/main/java/com/bawie/todaynews/fragment/MentLeftFragment.java
+++ app/src/main/java/com/bawie/todaynews/fragment/MentLeftFragment.java
@@ -3,11 +3,19 @@ package com.bawie.todaynews.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bawie.todaynews.MainActivity;
 import com.bawie.todaynews.R;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/5/8.
@@ -16,6 +24,7 @@ import com.bawie.todaynews.R;
  */
 
 public class MentLeftFragment extends Fragment {
+    private ImageView image;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -25,5 +34,45 @@ public class MentLeftFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        image= (ImageView) getActivity().findViewById(R.id.left_qqimage);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
+    }
+
+    private void login() {
+        UMShareAPI.get(getActivity()).getPlatformInfo(getActivity(), SHARE_MEDIA.QQ, new UMAuthListener() {
+            @Override
+            public void onStart(SHARE_MEDIA share_media) {
+
+            }
+
+            @Override
+            public void onComplete(SHARE_MEDIA share_media, int i, Map<String, String> map) {
+
+                Log.i("data",""+share_media);
+                String uid=map.get("uid");
+                String name=map.get("name");
+                String gender=map.get("gender");
+                String iconurl=map.get("iconurl");
+
+            }
+
+            @Override
+            public void onError(SHARE_MEDIA share_media, int i, Throwable throwable) {
+
+                Log.i("error",""+share_media);
+            }
+
+            @Override
+            public void onCancel(SHARE_MEDIA share_media, int i) {
+
+            }
+        });
     }
 }
